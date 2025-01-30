9d0fbee8be04631bcb51b87ba82db75b501af16e
unknown <ayshaschool57@gmail.com>
2025-01-30 15:08:18 -0500

    Add Validation JS file

---
+++ scripts/validation.js
@@ -0,0 +1,18 @@
+// enabling validation by calling enableValidation()
+// pass all the settings on call
+
+function enableValidation(config) {
+  const formEls = document.querySelectorAll("form");
+  console.log(formEls);
+}
+
+const config = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
+
+enableValidation(config);
