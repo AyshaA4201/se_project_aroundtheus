09c9236189b58fc54b7b08336799046aacfbc705
unknown <ayshaschool57@gmail.com>
2025-02-14 21:08:52 -0500

    Add function inputEls

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,12 +1,20 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
+function setEventListeners(formEl, options) {
+  const { inputSelector } = options;
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  console.log(inputEls);
+}
+
 function enableValidation(config) {
   const formEls = [...document.querySelectorAll(options.formSelector)];
   formEls.forEach((formEl) => {
     formEl.addEventListener("submit", (e) => {
       e.preventDefault();
     });
+
+    setEventListeners(formEl, options);
   });
 }
 
