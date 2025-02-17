3bfd84b595c96fcc1e375d4d447d07c9a1d78b19
unknown <ayshaschool57@gmail.com>
2025-02-17 16:18:55 -0500

    Add Popup Error span to Profile Modal

--- scripts/validation.js
+++ scripts/validation.js
@@ -4,11 +4,15 @@
 function setEventListeners(formEl, options) {
   const { inputSelector } = options;
   const inputEls = [...formEl.querySelectorAll(inputSelector)];
-  console.log(inputEls);
+  inputEls.forEach((inputEl) => {
+    inputEl.addEventListener("input", (e) => {
+      inputEl.validationMessage;
+    });
+  });
 }
 
 function enableValidation(config) {
-  const formEls = [...document.querySelectorAll(options.formSelector)];
+  const formEls = [...document.querySelectorAll(optionfas.formSelector)];
   formEls.forEach((formEl) => {
     formEl.addEventListener("submit", (e) => {
       e.preventDefault();
@@ -19,9 +23,9 @@ function enableValidation(config) {
 }
 
 const config = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__save",
   inactiveButtonClass: "popup__button_disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
