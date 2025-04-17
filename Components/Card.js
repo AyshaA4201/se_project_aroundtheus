6f6d5b9f709580ce17b995b9fda7a21909690eb9
Aysha <ayshaschool57@gmail.com>
2025-04-16 22:50:07 -0400

    Add Card Like/Delete Elements

--- Components/Card.js
+++ Components/Card.js
@@ -5,9 +5,37 @@ export default class Card {
     this._cardSelector = cardSelector;
   }
 
-  _setEventListeners() {}
+  _setEventListeners() {
+    this.cardElement
+      .querySelector(".card__like-button")
+      .addEventListener("click", () => {
+        this._handleLikeIcon();
+      });
+
+    this._cardElement
+      .querySelector(".card__delete-button")
+      .addEventListener("click", () => {
+        this._handleDeleteCard();
+      });
+  }
+
+  _handleDeleteCard() {
+    this._cardElement.remove();
+    this._cardElement = null;
+  }
+
+  _handleLikeIcon() {
+    this._cardElement
+      .querySelector("card__like-button")
+      .classList.toggle("card__like-button_is-active");
+  }
 
   getView() {
+    this._cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+    //get card view, set event listeners, and return the card element
     this._setEventListeners();
   }
 }
