114d9dafda645e2036b3837b56a5ed15b8494df8
Aysha <ayshaschool57@gmail.com>
2025-08-03 23:03:59 -0400

    Create Popup and PopupWithForm JS files

---
+++ src/scripts/Popup.js
@@ -0,0 +1,21 @@
+export default class Popup {
+  constructor({ popupSelector }) {
+    this._popupElement = document.querySelector(popupSelector);
+  }
+
+  open() {
+    // opens popup
+  }
+
+  close() {
+    // close popup
+  }
+
+  _handleEscClose() {
+    // listens for esc button
+  }
+
+  setEventListeners() {
+    // name explains it
+  }
+}
